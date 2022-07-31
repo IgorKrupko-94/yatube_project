f5f3202d2645597c6e958c516b520173f66aab5b
Игорь Крупко <igor.krupcko2010@andex.ru>
2022-07-31 18:17:11 +0300

    Последния изменения

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-
 from .models import Post, Group
+
 
 class PostAdmin(admin.ModelAdmin):
     # Перечисляем поля, которые должны отображаться в админке
@@ -11,6 +11,7 @@ class PostAdmin(admin.ModelAdmin):
     # Добавляем возможность фильтрации по дате
     list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
+
 
 # При регистрации модели Post источником конфигурации для неё назначаем
 # класс PostAdmin
--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -6,7 +6,9 @@ User = get_user_model()
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=50)
+    slug = models.SlugField(max_length=50,
+                            unique=True
+                            )
     description = models.TextField()
 
     def __str__(self):
@@ -26,6 +28,3 @@ class Post(models.Model):
                               on_delete=models.CASCADE,
                               related_name='posts'
                               )
-
-
-
