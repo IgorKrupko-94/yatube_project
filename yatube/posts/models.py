416924a3ba8619f7ee7c426555debe0aebf153fb
Игорь Крупко <igor.krupcko2010@andex.ru>
2022-08-17 23:23:52 +0300

    Исправлен файл models.py

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -30,8 +30,8 @@ class Post(models.Model):
                               related_name='posts'
                               )
 
-    def __str__(self):
-        return self.text[:20]
-
     class Meta:
         ordering = ['-pub_date']
+
+    def __str__(self):
+        return self.text[:20]
